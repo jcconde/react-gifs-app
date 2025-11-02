043a4cae693ce47769e72fa324f7cf2aefa555f3
Juanca <juancarlosc@onetree.com>
2025-11-02 14:27:26 -0400

    Display GIF file size in `GifsApp` list.

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -32,7 +32,7 @@ export const GifsApp = () => {
               <img src={gif.url} alt={gif.title}/>
               <h3>{gif.title}</h3>
               <p>
-                {gif.width} x {gif.height}
+                {gif.width} x {gif.height} (1.5mb)
               </p>
             </div>
           ))
