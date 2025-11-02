312215aeb1882a2ed468f15e8c024f3ee423c09a
Juanca <juancarlosc@onetree.com>
2025-11-02 15:24:05 -0400

    Add click handler for previous search terms in `GifsApp` and pass as prop to `PreviousSearches` component

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -3,8 +3,16 @@ import { CustomHeader } from "./shared/components/CustomHeader.tsx";
 import { SearchBar } from "./shared/components/SearchBar.tsx";
 import { PreviousSearches } from "./shared/gifs/components/PreviousSearches.tsx";
 import { GifList } from "./shared/gifs/components/GifList.tsx";
+import { useState } from "react";
 
 export const GifsApp = () => {
+
+  const [previousTerms, setPreviousTerms] = useState(['dragon ball z'])
+
+  const handleTermClicked = (term: string) => {
+    console.log({term});
+  }
+
   return (
     <>
       {/* Header */}
@@ -14,7 +22,10 @@ export const GifsApp = () => {
       <SearchBar placeholder="Buscar Gifs"/>
 
       {/* Busquedas previas */}
-      <PreviousSearches searches={["Goku", "Dragon Ball Z"]}/>
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
 
       {/* Gifs */}
       <GifList gifs={mockGifs}/>
