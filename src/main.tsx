3399e67e9335c8b4b41f203050e664b2c0bec906
Juanca <juancarlosc@onetree.com>
2025-11-10 11:22:27 -0400

    Introduce `MyCounterApp` with basic counter functionality and replace `GifsApp` in main entry point.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { MyCounterApp } from "./counter/components/MyCounterApp.tsx";
 import {GifsApp} from "./GifsApp.tsx";
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GifsApp/>
+    {/*<GifsApp/>*/}
+    <MyCounterApp/>
   </StrictMode>,
 )
