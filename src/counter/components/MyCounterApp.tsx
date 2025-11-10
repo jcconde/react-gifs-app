520c51f039eeb6e7863ef382094d4bbc3a82b96e
Juanca <juancarlosc@onetree.com>
2025-11-10 11:27:51 -0400

    Extract `useCounter` custom hook and refactor `MyCounterApp` to utilize it for improved reusability and cleaner component structure.

--- src/counter/components/MyCounterApp.tsx
+++ src/counter/components/MyCounterApp.tsx
@@ -1,20 +1,7 @@
-import { useState } from "react";
+import { useCounter } from "../hooks/useCounter.tsx";
 
 export const MyCounterApp = () => {
-  const [counter, setCounter] = useState(5);
-
-  const handleAdd = () => {
-    setCounter(counter + 1);
-  }
-
-  const handleSubtract = () => {
-    // setCounter(counter - 1);
-    setCounter((prevState) => prevState - 1);
-  }
-
-  const handleReset = () => {
-    setCounter(5);
-  }
+  const {counter, handleAdd, handleSubtract, handleReset} = useCounter(5);
 
   return (
     <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
